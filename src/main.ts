02306b61def4840f306abe3466a0af4fad4e8d47
Agustín La Valla <agulavalla@gmail.com>
2020-07-06 16:33:19 -0300

    Cors Enabled

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser'
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({limit:'50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-  app.enableCors();
+  // app.enableCors({origin:true});
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
